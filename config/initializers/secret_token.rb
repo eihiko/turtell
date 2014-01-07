96405c033c05f42b4318f01f4519ce1f2bb74d16
oeihiko <oeihiko@gmail.com>
2014-01-07 12:02:27 -0500

    Rails Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Turtell::Application.config.secret_key_base = '2fe4917993e683d98f38701e34958b15a10d2b1dd5ba6a82e571d96ac3a08d4a945e6760e2f2b7e1fe0728f66f531c45af0f129f0b457c98b27cd41392c6fd03'
